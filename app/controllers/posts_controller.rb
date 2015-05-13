faa1f362b0bd68a66ece429e540dbd69fa0f7b3a
arturo <arturo.alfaro87@gmail.com>
2015-05-12 21:31:56 -0300

    Added posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+	class PostsController < ApplicationController
+
+		def index
+			@posts= Post.all.order('created_at DESC')
+		end
+
+		def show
+			@post= Post.find(params[:id])
+		end
+		
+		def new 
+		end
+
+		def create
+			@post =Post.new(post_params)
+
+			@post.save
+
+			redirect_to @post
+		end
+
+# de aqui para abajo todo es privado, asi que cuidado con definir metodos.
+private
+
+def post_params
+	params.require(:post).permit(:title, :body)
+end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  resources :posts
+  root "posts#index"
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
